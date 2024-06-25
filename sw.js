01a8aa80be4d29bc1a74fad59f4a2ba715ecaac6
Mateus Isidoro <165904623+MateusIsidoro@users.noreply.github.com>
2024-06-25 13:22:56 -0300

    asdfghj

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
 workbox.routing.registerRoute(
     ({Request}) => Request.destination ==='image',
-    new workbox.strategies.CacheFisrt()
+    new workbox.strategies.NetworkFisrt()
 )
